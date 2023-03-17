d2d5e1b7c0d2cc85d17bcbd16bae2b51f4dd04a8
Sebastian Perez <spdseba@gmail.com>
2023-03-17 15:50:23 -0300

    Ultimas correcciones

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,137 +14,93 @@ let checkedInput = [];
 let inputSearch = document.getElementById('search-input');
 
 ///////////////////////////////////////////////////
-getDataFromApi()
+//Función asincrona que trae los eventos desde la API
 async function getDataFromApi() {
-    try {
-        const response = await fetch(urlAPI);
-        const data = await response.json();
-        let events = data.events;
-        
-        renderCategories(events);
-        renderCards(events);
-
-       // categoryContainer.appendChild(categoryFragment);
-        //Agrego Listeners a los input checkbox
-        let categoryCheckboxes = document.querySelectorAll("input[type=checkbox]");
-        categoryCheckboxes.forEach((checkbox) => checkbox.addEventListener('change', () =>
-        {
-          console.log("Checked")
-          //Convierto la NodeList en un array
-          checkedInput = Array.from(categoryCheckboxes).filter(checkbox => checkbox.checked).map( ele => ele.value);
-          console.log(checkedInput);
-          console.log(filterByAll(events, checkedInput))
-          renderCards(filterByAll(events, checkedInput));
-        }
-
-   
-        ));
-        inputSearch.addEventListener("keyup", () => {
-          renderCards(filterByAll(events, checkedInput));
-        });
-        /*
-        
-        let checkboxes = document.querySelectorAll('input[type=checkbox]');
-        checkboxes.forEach(checkbox => {
-            checkbox.addEventListener('change', () => {
-                chequeados = Array.from(checkboxes).filter(checkbox => checkbox.checked)
-                    .map(input => input.value);
-                console.log(`Acá están los ${chequeados}`);
-                cargarCard(filtrarTodo(eventos), 'cards');
-            })
-        });
-
-        inputSearch.addEventListener('keyup', () => {
-            cargarCard(filtrarTodo(eventos), 'cards');
-        })
-        */
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-
-
-
-/////////////////////////////////////////////////////
-/// OK  /////////////
-///////////////////////////////////////////////////
-function renderCategories(events){
-  console.log(events);
-    //recorro el arreglo de eventos y guardo las categorias que no esten repetidas
-    let arrayCategories = events.map( eventData => {
-      if(!categories.includes(eventData.category)){
-        categories.push(eventData.category)
+  try {
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+    let events = data.events;
+    renderCategories(events);
+    renderCards(events);
+    //Agrego Listeners a los input checkbox
+    let categoryCheckboxes = document.querySelectorAll("input[type=checkbox]");
+    categoryCheckboxes.forEach((checkbox) => checkbox.addEventListener('change', () =>
+      {
+        //Convierto la NodeList en un array
+        checkedInput = Array.from(categoryCheckboxes).filter(checkbox => checkbox.checked).map( ele => ele.value);
+        //LLamo a la función que renderiza las cards pasando por parametros los evetos filtrados
+        renderCards(filterByAll(events, checkedInput));
       }
+    ));
+    //Agrego el listener al input search
+    inputSearch.addEventListener("keyup", () => {
+      renderCards(filterByAll(events, checkedInput));
     });
-    let categoryFragment = document.createDocumentFragment();
-    for(let category of categories){
-      let div = document.createElement("div");
-      div.innerHTML = `
-          <div class="individual-check">
-            <label for="category1" class="mx-2">${category}</label>
-            <input type="checkbox" name="category" class="category-checkbox" value="${category}" id="${category}">
-          </div>
-      `;
-      categoryFragment.appendChild(div);
+  }
+  catch (error) {
+      console.log(error);
+  }
+}
+//Función que imprime las categorias que estan en la API
+function renderCategories(events){
+  //recorro el arreglo de eventos y guardo las categorias que no esten repetidas
+  let arrayCategories = events.map( eventData => {
+    if(!categories.includes(eventData.category)){
+      categories.push(eventData.category)
     }
-    console.log(categories);
-    categoryContainer.appendChild(categoryFragment);
-    
+  });
+  let categoryFragment = document.createDocumentFragment();
+  for(let category of categories){
+    let div = document.createElement("div");
+    div.innerHTML = `
+        <div class="individual-check">
+          <label for="category1" class="mx-2">${category}</label>
+          <input type="checkbox" name="category" class="category-checkbox" value="${category}" id="${category}">
+        </div>
+    `;
+    categoryFragment.appendChild(div);
+  }
+  categoryContainer.appendChild(categoryFragment);
 }
-//////////////////////////////////////////////////////////////////
-
+//Funcion que renderiza las cards de acuerdos a los checkbox seleccionados
 function renderCards(events, checkedInput){
-       // spinner.removeAttribute('hidden');
-       // spinner.setAttribute('hidden', '');
-       eventsCount = 0;
-       cards.innerHTML = "";
-        for (let event of events){
-          console.log("Entro")
-          eventsCount++;
-          let card = document.createElement('div');
-
-          card.className = "d-flex justify-content-center col-md-6 col-lg-4 py-5";
-          card.innerHTML = `
-          <div class="card flex-column justify-content-between col-md-6 col-lg-4">
-            <div class="card-image">
-              <img class="card-img-top" src="${event.image}" alt="Food Fair Image">
-            </div>
-            <div class="card-body  overflow-hidden">
-              <h5 class="card-title text-center">${event.name}</h5>
-              <h5 class="card-title text-center">${event.category}</h5>
-              <h6 class="card-subtitle mb-2 text-center p-3">${event.description}</h6>
-            </div>
-            <div class="card-footer d-flex justify-content-between align-items-center py-1">
-              <span>Price:$ ${event.price}</span>
-              <a href="./details.html?id=${event._id}" class="btn">See More</a>
-            </div>
-          </div>`;
-          fragment.appendChild(card);
-        }
-        cards.appendChild(fragment);
-         //Si no se encontraron eventos se imprime un mensaje en pantalla
-      if(eventsCount == 0){
-        let card = document.createElement('div');
-        card.className = "d-flex justify-content-center align-items-center  h-100 w-100";
-        card.innerHTML = `<div class="alert alert-primary" role="alert">
-        Events not found
-      </div>`;
-        fragment.appendChild(card);
-        cards.appendChild(fragment);
-        console.log("no hay")
-      }     
+  // spinner.removeAttribute('hidden');
+  // spinner.setAttribute('hidden', '');
+  eventsCount = 0;
+  cards.innerHTML = "";
+  for (let event of events){
+    eventsCount++;
+    let card = document.createElement('div');
+    card.className = "d-flex justify-content-center col-md-6 col-lg-4 py-5";
+    card.innerHTML = `
+    <div class="card flex-column justify-content-between col-md-6 col-lg-4">
+      <div class="card-image">
+        <img class="card-img-top" src="${event.image}" alt="Food Fair Image">
+      </div>
+      <div class="card-body  overflow-hidden">
+        <h5 class="card-title text-center">${event.name}</h5>
+        <h6 class="card-subtitle mb-2 text-center p-3">${event.description}</h6>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center py-1">
+        <span>Price:$ ${event.price}</span>
+        <a href="./details.html?id=${event._id}" class="btn">See More</a>
+      </div>
+    </div>`;
+    fragment.appendChild(card);
+  }
+  cards.appendChild(fragment);
+    //Si no se encontraron eventos se imprime un mensaje en pantalla
+  if(eventsCount == 0){
+    let card = document.createElement('div');
+    card.className = "d-flex justify-content-center align-items-center  h-100 w-100";
+    card.innerHTML = `<div class="alert alert-primary" role="alert">
+    Events not found
+    </div>`;
+    fragment.appendChild(card);
+    cards.appendChild(fragment);
+  }     
 }
-
-
-///////////////////////////////////////////////////////////
-
-const searchBtn = document.getElementById("search-button");
-//searchBtn.addEventListener("click", findCards);
-
-const searchInput = document.getElementById("search-input");
-
-
+//Funcion que filtra los eventos de la API de acuerdo a los checkbox seleccionados
 function filterByCheckbox(checkBoxesArray, events){
   if(checkBoxesArray != 0){
     return events.filter(evt => checkBoxesArray.includes(evt.category));
@@ -152,6 +108,7 @@ function filterByCheckbox(checkBoxesArray, events){
     return events;
   }
 }
+//Funcion que filtra los eventos de la API de acuerdo al texto ingresado en el input search
 function filterBySearch(searchValue, events){
   if(searchValue != ""){
     return events.filter(evt => evt.name.toLowerCase().includes(searchValue.toLowerCase().trim()));
@@ -160,11 +117,11 @@ function filterBySearch(searchValue, events){
   }
 }
 
-
+//Función que filtra los eventos por dos criterios(de acuerdo al los chechbox seleccionados y por lo escrito en en input search)
 function filterByAll(events, checkboxArray){
-
   let checkboxFiltered = filterByCheckbox(checkboxArray, events);
   let eventsFiltered = filterBySearch(inputSearch.value, checkboxFiltered);
-
   return eventsFiltered;
 }
+//Llamo a la función encargada de traer los datos de la API y mostrarlos en pantalla
+getDataFromApi()
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,13 +1,12 @@
 let table = document.getElementById("table");
 //URL de la API de donde se van a obtener los eventos
 let urlAPI = "https://mindhub-xj03.onrender.com/api/amazing";
-
 let fragment = document.createDocumentFragment();
+//Funcion asincrona que obtiene los eventos de la API y los muestra en pantalla
 async function getDataFromApi(urlAPI) {
   try {
       const response = await fetch(urlAPI)
       const data = await response.json();
-      console.log(data)
       evtMaxAtt_evtMinAtt_evtMaxCap(data);
       upcomingEvt_by_category(data);
       pastEvt_by_category(data);
@@ -17,35 +16,33 @@ async function getDataFromApi(urlAPI) {
       console.error('Error: '+error);
   }    
 }
-
+//Función que muestra en pantalla la primer tabla 
 function renderTableEvtStatistics(evtMaxAtt, evtMinAtt, evtMaxCapacity){
-
-  console.log("TAble")
-     let card = document.createElement('div');
-
-     card.className = "";
-     card.innerHTML = `
-     <h2 class="table-title py-1">Events Statistics</h2>
-     <table class="table">
-       <thead class="">
-           <tr>
-               <th scope="col">Event with the highest percentage of attendance</th>
-               <th scope="col">Event with the lowest percentage of attendance</th>
-               <th scope="col">Event with larger capacity</th>
-           </tr>
-       </thead>
-       <tbody>
-         <tr>
-           <td>${evtMaxAtt.name} (${evtMaxAtt.maxAtt.toFixed(2)}%)</td>
-           <td>${evtMinAtt.name} (${evtMinAtt.minAtt.toFixed(2)}%)</td>
-           <td>${evtMaxCapacity.name} (${evtMaxCapacity.maxCapacity})</td>
-         </tr>
-       </tbody>
-     </table>`;
-     fragment.appendChild(card);
-   //}
-   table.appendChild(fragment);
+  let card = document.createElement('div');
+  card.className = "";
+  card.innerHTML = `
+  <h2 class="table-title py-1">Events Statistics</h2>
+  <table class="table">
+    <thead class="">
+        <tr>
+            <th scope="col">Event with the highest percentage of attendance</th>
+            <th scope="col">Event with the lowest percentage of attendance</th>
+            <th scope="col">Event with larger capacity</th>
+        </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>${evtMaxAtt.name} (${evtMaxAtt.maxAtt.toFixed(2)}%)</td>
+        <td>${evtMinAtt.name} (${evtMinAtt.minAtt.toFixed(2)}%)</td>
+        <td>${evtMaxCapacity.name} (${evtMaxCapacity.maxCapacity})</td>
+      </tr>
+    </tbody>
+  </table>`;
+  fragment.appendChild(card);
+  table.appendChild(fragment);
 }
+//Función que obtiene el evento con mayoy y menor Attendance y con maxima capacidad 
+//Y luego llama a la función que muestra la tabla en pantalla
 function evtMaxAtt_evtMinAtt_evtMaxCap(data){
   let maxAtt = 0;
   let attendance = 0;
@@ -54,73 +51,64 @@ function evtMaxAtt_evtMinAtt_evtMaxCap(data){
   let evtMaxAtt = {};
   let evtMinAtt = {};
   let evtMaxCapacity = {};
-      for(let evt of data.events){
-        //console.log(evt)
-        if(evt.assistance){
-          attendance = (parseInt(evt.assistance) * 100) / parseInt(evt.capacity);
-          if(attendance >= maxAtt){
-            maxAtt = attendance;
-            evtMaxAtt = evt;
-            evtMaxAtt["maxAtt"] = attendance;
-          }
-          if(attendance <= minAtt){
-            minAtt = attendance
-            evtMinAtt = evt;
-            evtMinAtt["minAtt"] = attendance
-          }
+    for(let evt of data.events){
+      if(evt.assistance){
+        attendance = (parseInt(evt.assistance) * 100) / parseInt(evt.capacity);
+        if(attendance >= maxAtt){
+          maxAtt = attendance;
+          evtMaxAtt = evt;
+          evtMaxAtt["maxAtt"] = attendance;
         }
-        if(evt.capacity > maxCapacity){
-          maxCapacity = evt.capacity;
-          evtMaxCapacity = evt;
-          evtMaxCapacity["maxCapacity"] = maxCapacity;
+        if(attendance <= minAtt){
+          minAtt = attendance
+          evtMinAtt = evt;
+          evtMinAtt["minAtt"] = attendance
         }
       }
-      console.log(evtMaxAtt);
-      console.log(evtMinAtt);
-      console.log(evtMaxCapacity);
-      renderTableEvtStatistics(evtMaxAtt, evtMinAtt, evtMaxCapacity);
-}
-
-function renderTableUpEvtByCategory(categories){
-
-     let card = document.createElement('div');
-     card.className = "";
-     let template2 = "";
-     let keysCat = Object.keys(categories);
-     console.log(keysCat)
-     for(let i = 0; i < keysCat.length ; i++){
-      console.log(categories[keysCat[i]])
-      console.log(categories[keysCat[i]].estimate)
-      if(categories[keysCat[i]].revenue > 0){
-        template2 +=
-        `<tr>
-          <td>${keysCat[i]}</td>
-          <td>${categories[keysCat[i]].revenue}</td>
-          <td>${((categories[keysCat[i]].estimate * 100) / categories[keysCat[i]].capacity).toFixed(2) } %</td>
-        </tr>`
+      if(evt.capacity > maxCapacity){
+        maxCapacity = evt.capacity;
+        evtMaxCapacity = evt;
+        evtMaxCapacity["maxCapacity"] = maxCapacity;
       }
     }
-     let template = `
-     <h2 class="table-title py-1">Upcoming events statistics by category</h2>
-     <table class="table">
-       <thead class="">
-           <tr>
-            <th scope="col">Categories</th>
-            <th scope="col">Revenues</th>
-            <th scope="col">Percentage of attendance</th>
-           </tr>
-       </thead>
-       <tbody>
-          ${template2}
-       </tbody>
-     </table>`
-     card.innerHTML = template;
-     fragment.appendChild(card);
-   table.appendChild(fragment);
+    renderTableEvtStatistics(evtMaxAtt, evtMinAtt, evtMaxCapacity);
 }
-
+//Función que muestra en pantalla la Tabla de eventos por venir ordenados por categoría
+function renderTableUpEvtByCategory(categories){
+  let card = document.createElement('div');
+  card.className = "";
+  let template2 = "";
+  let keysCat = Object.keys(categories);
+  for(let i = 0; i < keysCat.length ; i++){
+    if(categories[keysCat[i]].revenue > 0){
+      template2 +=
+      `<tr>
+        <td>${keysCat[i]}</td>
+        <td>$ ${categories[keysCat[i]].revenue}</td>
+        <td>${((categories[keysCat[i]].estimate * 100) / categories[keysCat[i]].capacity).toFixed(2) } %</td>
+      </tr>`
+    }
+  }
+  let template = `
+  <h2 class="table-title py-1">Upcoming events statistics by category</h2>
+  <table class="table">
+    <thead class="">
+        <tr>
+        <th scope="col">Categories</th>
+        <th scope="col">Revenues</th>
+        <th scope="col">Percentage of attendance</th>
+        </tr>
+    </thead>
+    <tbody>
+      ${template2}
+    </tbody>
+  </table>`
+  card.innerHTML = template;
+  fragment.appendChild(card);
+  table.appendChild(fragment);
+}
+//Función que obtiene las categorias de los eventos de la API y les agrega un objeto con las keys para guardar los valores
 function generateCategoryObject(data){
-  console.log(data);
   let categories = {}
   for(let evt of data.events){
     if(!categories.hasOwnProperty(evt.category)){
@@ -134,90 +122,70 @@ function generateCategoryObject(data){
   }
   return categories;
 }
-
+//Función que obtiene los eventos por venir con sus datos para luego llamar a la función que los muestra en una tabla
 function upcomingEvt_by_category(data){
   let categoriesData = generateCategoryObject(data);
-    console.log("datos"+data.currentDate);
-
-    currentDate = data.currentDate;
-    //let currentDate = new Date(data.currentDate);
-    console.log(currentDate);
-    for(let evt of data.events){
-      if(evt.date > currentDate){
-        console.log("Date")
-        categoriesData[evt.category].revenue += parseInt(evt.estimate) * parseInt(evt.price)
-        categoriesData[evt.category].estimate += parseInt(evt.estimate)
-        categoriesData[evt.category].capacity += parseInt(evt.capacity)
+  currentDate = data.currentDate;
+  //let currentDate = new Date(data.currentDate);
+  for(let evt of data.events){
+    if(evt.date > currentDate){
+      categoriesData[evt.category].revenue += parseInt(evt.estimate) * parseInt(evt.price)
+      categoriesData[evt.category].estimate += parseInt(evt.estimate)
+      categoriesData[evt.category].capacity += parseInt(evt.capacity)
     }
 
   }
   renderTableUpEvtByCategory(categoriesData);
 
 }
-
-
-
+//Función que genera una tabla en pantalla con los eventos pasados ordenados por categorias 
 function renderTablePastEvtByCategory(categories){
-  console.log("TAble")
-     // console.log("Entro")
-     let card = document.createElement('div');
-     card.className = "";
-     let template2 = "";
-    // console.log(categories)
-     let keysCat = Object.keys(categories);
-     console.log(keysCat)
-     for(let i = 0; i < keysCat.length ; i++){
-      console.log(categories[keysCat[i]])
-      console.log(categories[keysCat[i]].estimate)
-      if(categories[keysCat[i]].revenue > 0){
-        console.log(categories[keysCat[i]].eventCount);
-        template2 +=
-        `<tr>
-          <td>${keysCat[i]}</td>
-          <td>${categories[keysCat[i]].revenue}</td>
-          <td>${(((categories[keysCat[i]].assistance / categories[keysCat[i]].capacity * 100) )).toFixed(2) } %</td>
-        </tr>`
-      }
+  let card = document.createElement('div');
+  card.className = "";
+  let template2 = "";
+  let keysCat = Object.keys(categories);
+  for(let i = 0; i < keysCat.length ; i++){
+    if(categories[keysCat[i]].revenue > 0){
+      template2 +=
+      `<tr>
+        <td>${keysCat[i]}</td>
+        <td>$ ${categories[keysCat[i]].revenue}</td>
+        <td>${(((categories[keysCat[i]].assistance / categories[keysCat[i]].capacity * 100) )).toFixed(2) } %</td>
+      </tr>`
     }
-     let template = `
-     <h2 class="table-title py-1">Past events statistics by category</h2>
-     <table class="table">
-       <thead class="">
-           <tr>
-            <th scope="col">Categories</th>
-            <th scope="col">Revenues</th>
-            <th scope="col">Percentage of attendance</th>
-           </tr>
-       </thead>
-       <tbody>
-          ${template2}
-       </tbody>
-     </table>`
-     card.innerHTML = template;
-     fragment.appendChild(card);
-   //}
-   table.appendChild(fragment);
+  }
+  let template = `
+  <h2 class="table-title py-1">Past events statistics by category</h2>
+  <table class="table">
+    <thead class="">
+        <tr>
+        <th scope="col">Categories</th>
+        <th scope="col">Revenues</th>
+        <th scope="col">Percentage of attendance</th>
+        </tr>
+    </thead>
+    <tbody>
+      ${template2}
+    </tbody>
+  </table>`
+  card.innerHTML = template;
+  fragment.appendChild(card);
+  table.appendChild(fragment);
 }
-
+//Función que obtiene los eventos pasados con sus datos para luego llamar a la función que los muestra en una tabla
 function pastEvt_by_category(data){
   let categoriesData = generateCategoryObject(data);
-    console.log("datos"+data.currentDate);
-
     currentDate = data.currentDate;
     //let currentDate = new Date(data.currentDate);
-    console.log(currentDate);
     for(let evt of data.events){
       if(evt.date < currentDate){
-        console.log("Date")
         categoriesData[evt.category].revenue += parseInt(evt.assistance) * parseInt(evt.price);
         categoriesData[evt.category].assistance += parseInt(evt.assistance);
         categoriesData[evt.category].capacity += parseInt(evt.capacity);
     }
-
   }
   renderTablePastEvtByCategory(categoriesData);
-
 }
-
+//Funcion que obtiene los eventos de la API y los muestra en tablas en pantalla
 getDataFromApi(urlAPI);
 
